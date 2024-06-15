aaf6726f08b6c00cc507c4c398a55b55508ccf49
LGGM-DEV-BR <137808191+LGGM-DEV-BR@users.noreply.github.com>
2024-06-15 11:41:22 -0300

    Solved C exercise N1

--- programa01.c
+++ programa01.c
@@ -2,6 +2,11 @@
 
 int main(void) {
   int number;
+  printf("Choose a number: ");
   scanf("%d", &number);
-  printf("You entered: %d\n", number);
+  if (number % 5 == 0 && number % 3 == 0) {
+    printf("Your number is divisible by 5 and 3!\n");
+  } else {
+    printf("Your number isn't divisible by 5 and 3!\n");
+  }
 }
